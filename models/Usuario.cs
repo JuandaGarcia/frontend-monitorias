6f09ab0ef8e3c3743ce8dd76dedcc71aa14841bc
catmendoza <mendo657@gmail.com>
2020-11-27 03:37:03 -0500

    requerimientos y encriptacion de contraseña

--- models/Usuario.cs
+++ models/Usuario.cs
@@ -13,15 +13,14 @@ namespace Frontend.Pages
 
         public string career { get; set; }
         [Required]
-        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Correo no valido")]
 
         public string mail { get; set; }
         [Required(ErrorMessage = "La cedula es requerido")]
-        [StringLength(10, ErrorMessage = "La contraseña tiene que tener maximo 10 caracteres.")]
+
 
         public string code { get; set; }
         [Required(ErrorMessage = "La contraseña es requerido")]
-        [StringLength(8, ErrorMessage = "La contraseña tiene que tener maximo 8 caracteres.")]
 
         public string password { get; set; }
 
