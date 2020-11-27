64db53ed0eaeb1eb5d11ef64c33485316023143b
Juan David <gottygarcia@gmail.com>
2020-11-27 03:57:52 -0500

    publicaciones

---
+++ models/Comentario.cs
@@ -0,0 +1,11 @@
+namespace Frontend.Pages
+{
+    public class Comentario
+    {
+       
+        public string Id { get; set; }
+        public string description { get; set; }
+        public string idMonitoria { get; set; }
+
+    }
+}
